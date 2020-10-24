b956cbb8753fb1374d485e9d05973a4a7fefcce4
Thiefre <kevinvluu@gmail.com>
2020-10-24 00:42:47 -0700

    Finished vertical segmentation

--- ECG Digitization/ECG Code/RegionSegmentation.py
+++ ECG Digitization/ECG Code/RegionSegmentation.py
@@ -8,8 +8,24 @@ Created on Fri Oct  9 22:39:58 2020
 import cv2
 import numpy as np
 import scipy.ndimage as ndimage
+import math
+import os
 
+path = 'vertically_segmented'
+try:
+    files = os.listdir(path)
+    count = len(files)+1
+except FileNotFoundError:
+    os.mkdir(path)
+    count = 1
+
+index = 0
+#Input image
 img = cv2.imread('rotated_houghtransform.jpg')
+
+#Thresholded image
+threshimg = cv2.imread('red_channel_thresholded/red_channel_thresholded2.jpg')
+height, width, channels = img.shape
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -19,29 +35,40 @@ mask = cv2.inRange(hsv, lower_range, upper_range)
 
 mask = 255-mask
 
-cv2.imshow('mask',mask)
-cv2.waitKey()
-
+count = 0
+pointset = list()
 edges = cv2.Canny(mask,50,150,apertureSize = 3)
+lines= cv2.HoughLines(edges, 1, np.pi/180.0, 165, np.array([]), 0, 0)
 
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
-for line in lines:
-    for rho,theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
+a,b,c = lines.shape
+for i in range(a):
+    rho = lines[i][0][0]
+    theta = lines[i][0][1]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0, y0 = a*rho, b*rho
+    pt1 = ( int(x0+1000*(-b)), int(y0+1000*(a)) )
+    pt2 = ( int(x0-1000*(-b)), int(y0-1000*(a)) )
+    # cv2.line(img, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA)
+    add = True
+    for points in pointset:
+        if abs(points[0] - pt1[0]) < 50:
+            add = False
+            break
+        else:
+            add = True
+    if add == True:
+        pointset.append(pt1)
         
-        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-        # crop_img = img[0:y1, 0:x1]
-        # cv2.imshow('cropped',crop_img)
-        # cv2.waitKey()
+pointset.sort()
+oldX = 0
+for points in pointset:
+    crop_img = threshimg[0:points[1], oldX:points[0]]
+    oldX = points[0]
+    cv2.imwrite(path + '/'+ path + str(count)+'_'+str(index)+'.jpg', crop_img)
+    index += 1
+crop_img = threshimg[0: height, oldX: width]
+cv2.imwrite(path + '/'+ path + str(count)+'_'+str(index)+'.jpg', crop_img)
 
-cv2.imshow('img', img)
-cv2.waitKey()
 #Writes the image to destination
 # cv2.imwrite('blue.jpg', img)
