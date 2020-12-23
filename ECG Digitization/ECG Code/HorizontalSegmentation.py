5e245f483037344da2a3cbf602d8301b9a863600
Thiefre <kevinvluu@gmail.com>
2020-12-23 12:20:22 -0800

    Interpolation

--- ECG Digitization/ECG Code/HorizontalSegmentation.py
+++ ECG Digitization/ECG Code/HorizontalSegmentation.py
@@ -10,23 +10,23 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
-path = 'horizontally_segmented'
+dest = 'horizontally_segmented'
 try:
-    files = os.listdir(path)
+    files = os.listdir(dest)
 except FileNotFoundError:
-    os.mkdir(path)
+    os.mkdir(dest)
 
-folder = 'vertically_segmented'
+src = 'vertically_segmented'
 try:
-    vertical_files = os.listdir(folder)
+    vertical_files = os.listdir(src)
 except FileNotFoundError:
-    os.mkdir(path)
+    os.mkdir(dest)
     
 index = 0
 for item in vertical_files:
     
     # Load as greyscale
-    img = cv2.imread(os.path.join(folder,item))
+    img = cv2.imread(os.path.join(src,item))
     invertedimg = 255-img
     
     height, width = img.shape[:2]
@@ -47,13 +47,14 @@ for item in vertical_files:
             pass
         # If that row is not whitespace, and there has been consecutively at least 30 whitespaces before, start the crop
         # This will only enter at the start of a signal; at the start of the next signal it will crop
+        # 30 is how many pixels there can be before something can be apart of a crop; the lower the number the less will be included
         elif count-start >= 30 and proj[i][0] > 1500:
             # If the cropping image is bigger than 50 pixels in height, then crop
             if (start - startCrop >= 30):
                 if((count-start)/2)+start <= height:
                         # Crops the image based on the start of the next signal, the start of the previous whitespace, and the inital cropping index
                         crop_img = img[startCrop:start, 0:width]
-                        cv2.imwrite(path + '/'+ item[:len(item)-4] +'_'+str(index)+'.jpg', crop_img)
+                        cv2.imwrite(dest + '/'+ item[:len(item)-4] +'_'+str(index)+'.jpg', crop_img)
                         index += 1
             # Sets the index to start cropping
             count = i
@@ -65,7 +66,7 @@ for item in vertical_files:
             start = i
     if( start - startCrop >= 30):
         crop_img = img[startCrop:start, 0:width]
-        cv2.imwrite(path + '/'+ item[:len(item)-4] +'_'+str(index)+'.jpg', crop_img)
+        cv2.imwrite(dest + '/'+ item[:len(item)-4] +'_'+str(index)+'.jpg', crop_img)
     index = 0
 # Save result
-# cv2.imwrite(path + '/'+ path + str(count)+'_'+str(index)+'.jpg', result)
+# cv2.imwrite(dest + '/'+ dest + str(count)+'_'+str(index)+'.jpg', result)
